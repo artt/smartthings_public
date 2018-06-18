f6ae17e05e40e9d5d2cc157e83d548b732e895d2
fishix10 <art.tosborvorn@gmail.com>
2018-06-19 00:40:48 +0700

    v

---
+++ test_switch.groovy
@@ -0,0 +1,45 @@
+preferences {    
+	section("Internal Access"){
+		input "id_pw", "text", title: "ID & PW for RM Plugin or RM Bridge(Optional)", required: false
+		input "internal_ip", "text", title: "IP for RM Plugin or RM Bridge or HA(Required for all)", required: true
+		input "internal_port", "text", title: "Port(Required for all)", required: true
+		input "internal_on_path", "text", title: "On Path(Required for all)", required: true
+        input "internal_off_path", "text", title: "Off Path", required: false
+	}
+}
+
+metadata {
+	definition (name: "Test Switch", namespace: "fishix10", author: "fishix10") {
+		capability "Actuator"
+		capability "Switch"
+		capability "Momentary"
+		capability "Sensor"
+        
+		attribute "about", "string"
+	}
+
+	// simulator metadata
+	simulator {
+	}
+
+	tiles(scale: 2) {
+		standardTile(name: "switch_on", type: "generic", width: 2, height: 2) {
+			label: 'On', action: "on"
+		}
+		standardTile(name: "switch_off", type: "generic", width: 2, height: 2) {
+			label: 'Off', action: "off"
+		}
+		main "switch_on"
+		details(["switch_on, switch_off"])
+	}
+}
+
+def on() {
+	log.debug "---ON COMMAND---"
+}
+
+def off() {
+	log.debug "---ON COMMAND---"
+}
+
+def push() {}
